fcadc05e2b5492dd6d3809fc523e8299953c0bda
dopv <dopv@vicoders.com>
2022-03-23 06:40:30 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+
 
 function App() {
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/auth/login',{
+      method: "POST",
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({username: "user", password: "secret"})
+    }).then(res=>res.json())
+    .then(data=>{
+      console.log('data', data);
+    })
+    .catch((e)=>{
+      console.log('errr', e);
+    })
+  }, [])
+  
   return (
     <div className="App">
       <header className="App-header">
